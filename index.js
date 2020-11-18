2f4a0ade8b3d55ad9cbc39ff237b8b8444180fb4
Pixelshot <hazrijordan@gmail.com>
2020-11-18 09:13:01 +0800

    Initial commit, section one is done

---
+++ index.js
@@ -0,0 +1,31 @@
+// PARALLAX
+let scene = document.getElementById('scene');
+let parallaxInstance = new Parallax(scene);
+
+// SWIPER
+
+let keys = [
+  'Mercury',
+  'Venus',
+  'Earth',
+  'Mars',
+  'Jupiter',
+  'Saturn',
+  'Uranus',
+  'Neptune',
+];
+
+let slider = new Swiper('.swiper-container', {
+  slidesPerView: 'auto',
+  spaceBetween: 150,
+  centeredSlides: true,
+  mousewheel: true,
+  pagination: {
+    el: '.planet-links',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<div class="' + className + '">' + keys[index] + '</div>';
+      // return `<div class=${className}>${keys[index]}</div>`;
+    },
+  },
+});
